9491de98a2ec8626678e22b4325fdf18a65e05b6
Abdou <abdousani60@gmail.com>
2026-03-02 14:33:00 +0100

    feat: ajout de la mutation `signUp` dans `authQueries` avec `react-query`

--- src/features/auth/hooks/queries/auth.ts
+++ src/features/auth/hooks/queries/auth.ts
@@ -1,7 +1,11 @@
-import {queryOptions} from "@tanstack/react-query";
+import {mutationOptions, queryOptions} from "@tanstack/react-query";
+import {signUp} from '../../services/auth.service.ts'
 
 export const authQueries = {
     check: () => queryOptions({
         queryKey: ['auth', 'check'],
     }),
+    signUp: () => mutationOptions({
+        mutationFn: signUp
+    })
 }
