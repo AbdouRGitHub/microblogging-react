b4a1e5f72485274c3622d8b8a66441c74ff48583
Abdou <abdousani60@gmail.com>
2026-03-16 15:28:32 +0100

    feat: ajout du menu contextuel à trois points dans `PostFeedCard` pour la modification, la suppression et l'ajout aux signets des posts et mise à jour des styles associés

--- src/features/post/components/PostFeedCard.tsx
+++ src/features/post/components/PostFeedCard.tsx
@@ -2,6 +2,8 @@ import styles from "../styles/PostFeedCard.module.css";
 import {formatShortDate} from "../../../shared/utils/formatShortDate.ts";
 import {Link} from "react-router";
 import {faker} from "@faker-js/faker";
+import {Bookmark, Ellipsis, Pencil, Trash} from "lucide-react";
+import {useState} from "react";
 
 function PostFeedCard({id, userId, content, likes, comments, username, createdAt, width = "80%"}: {
     id: string,
@@ -13,6 +15,7 @@ function PostFeedCard({id, userId, content, likes, comments, username, createdAt
     createdAt: string,
     width?: string
 }) {
+    const [ellipsisCtxMenu, setEllipsisCtxMenu] = useState<boolean>(false);
     return (
         <article className={styles.card} style={{width: `${width}`}}>
             <div className={styles.header}>
@@ -22,6 +25,23 @@ function PostFeedCard({id, userId, content, likes, comments, username, createdAt
                     </Link>
                     <p> {username} <span
                         style={{color: 'grey', fontSize: 'small'}}> . {formatShortDate(new Date(createdAt))}</span></p>
+                </div>
+                <div className={styles.ellipsisContainer}>
+                    <Ellipsis className={styles.ellipsis}
+                              onClick={(e) => {
+                                  e.stopPropagation();
+                                  setEllipsisCtxMenu(!ellipsisCtxMenu);
+                              }}/>
+                    {
+                        ellipsisCtxMenu && (
+                            <div className={styles.ellipsisCtxMenu}>
+                                    <span> <Pencil/> Modifier le post </span>
+                                    <span> <Bookmark/> Ajouter aux signets</span>
+                                    <span style={{ color: "red" }}> <Trash/> Supprimer le post</span>
+                            </div>
+                        )
+                    }
+
                 </div>
             </div>
             <Link to={`/post/${id}`}
